9bfd8c053e737fd32f9a309a8577b540f95128e3
Thomas Nilefalk <thomas@junovagen.se>
2019-03-05 23:50:14 +0000

    Create the Tunes component

---
+++ src/components/Tunes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Tunes</h1>
+    <ul>
+      <li v-for="tune in tunes" :key="tune">{{ tune }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Tunes extends Vue {
+  private data() {
+    return {
+      tunes: ["Hello Dolly", "Boogie Woogie Choo-Choo Train"]
+    };
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Tunes/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Tunes from "@/components/Tunes.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    Tunes
   }
 })
 export default class Home extends Vue {}

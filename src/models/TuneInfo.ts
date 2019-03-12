82d376e56c523b65658d00a114687e867022e22f
Thomas Nilefalk <thomas@junovagen.se>
2019-03-12 08:43:19 +0100

    Add an album art field

--- src/models/TuneInfo.ts
+++ src/models/TuneInfo.ts
@@ -4,18 +4,21 @@ export class TuneInfo {
   public album: string;
   public genre: string;
   public bpm: number;
+  public art: string;
 
   constructor(
     title: string,
     artist: string = 'unknown',
     album: string = 'unknown',
     genre: string = 'unknown',
-    bpm: number = 0
+    bpm: number = 0,
+    art: string = 'https://picsum.photos/200/?random'
   ) {
     this.title = title;
     this.artist = artist;
     this.album = album;
     this.genre = genre;
     this.bpm = bpm;
+    this.art = art;
   }
 }

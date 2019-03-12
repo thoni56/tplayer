daee051df98661c457964df264e0829e4df8926e
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2019-03-12 16:52:21 +0100

    Add buttons for filtering

--- src/views/Filtering.vue
+++ src/views/Filtering.vue
@@ -1,7 +1,17 @@
 <template>
   <v-container>
     <h1>Filter</h1>
-    <Tunes/>
+    <v-layout row wrap justify-center>
+      <v-flex xs12 sm6 class="py-2">
+        <v-btn-toggle v-model="genre_selected" multiple>
+          <v-btn flat>Bugg</v-btn>
+          <v-btn flat>Boogie Woogie</v-btn>
+          <v-btn flat>Lindy Hop</v-btn>
+          <v-btn flat>West Coast Swing</v-btn>
+        </v-btn-toggle>
+      </v-flex>
+    </v-layout>
+    <Tunes />
   </v-container>
 </template>
 
@@ -9,11 +19,19 @@
 import Vue from "vue";
 import Component from "vue-class-component";
 import Tunes from "@/components/Tunes.vue";
+import { TuneInfo } from '@/models/TuneInfo';
 
 @Component({
   components: {
     Tunes
   }
 })
-export default class Filtering extends Vue {}
+export default class Filtering extends Vue {
+  public data () {
+      return {
+        genre_selected: [],  // Nothing selected
+        tunes: [new TuneInfo("Hello Dolly"), new TuneInfo("Big Wheel")]
+      }
+    }
+}
 </script>

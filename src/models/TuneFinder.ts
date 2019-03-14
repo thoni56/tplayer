89578a825d4d33320c0997c784e5676c38aab942
Thomas Nilefalk <thomas@junovagen.se>
2019-03-14 07:46:16 +0100

    Add emitter.end() when done walking

--- src/models/TuneFinder.ts
+++ src/models/TuneFinder.ts
@@ -7,11 +7,13 @@ export default class TuneFinder extends Vue {
     private tunes: TuneInfo[] = [];
 
     public findTunesFrom(directory: string) {
-        const emitter = walk('C:/Users/Thomas/Music/iTunes/iTunes Media/Music', {});
+        const emitter = walk('C:/Users/Thomas/Music/iTunes/iTunes Media/Music');
         emitter.on('file', (path, stat) => {
             this.addTune(path);
         })
-        emitter.on('end', () => { ; })
+        emitter.on('end', () => {
+            emitter.end();
+        })
         emitter.on('error', (path: string, error: string) => {
             console.log('ERROR: ', error)
         })

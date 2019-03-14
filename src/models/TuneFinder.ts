d5484649c3ffed3fdf9409df46ae699a78da12cc
Thomas Nilefalk <thomas@junovagen.se>
2019-03-14 17:51:39 +0100

    Add type to emitter parameter

--- src/models/TuneFinder.ts
+++ src/models/TuneFinder.ts
@@ -1,6 +1,6 @@
 import walk from 'walkdir';
 import { TuneInfo } from './TuneInfo';
-import { Vue, Emit } from 'vue-property-decorator';
+import { Vue } from 'vue-property-decorator';
 import { EventBus } from '@/EventBus';
 
 export default class TuneFinder extends Vue {
@@ -8,7 +8,7 @@ export default class TuneFinder extends Vue {
 
     public findTunesFrom(directory: string) {
         const emitter = walk('C:/Users/Thomas/Music/iTunes/iTunes Media/Music');
-        emitter.on('file', (path, stat) => {
+        emitter.on('file', (path: string, stat) => {
             this.addTune(path);
         })
         emitter.on('end', () => {

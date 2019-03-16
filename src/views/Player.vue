c0a17b7727e671a74f2f8c18920c602eb35271aa
Thomas Nilefalk <thomas@junovagen.se>
2019-03-16 20:12:08 +0100

    Add play and pause buttons (non-working for now)

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -1,13 +1,19 @@
 <template>
   <v-container>
     <h1>PLAYER</h1>
+    <v-btn v-on="play">Play</v-btn>
+    <v-btn v-on="pause">Pause</v-btn>
   </v-container>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
-import Tunes from "@/components/Tunes.vue";
+import { Component, Vue, Prop } from "vue-property-decorator";
+import TuneInfo from "TuneInfo.vue";
 
 @Component
-export default class Player extends Vue {}
+export default class Player extends Vue {
+  @Prop() tunes!: TuneInfo[];
+  public play() {}
+  public pause() {}
+}
 </script>

4f0ae3dd0685cc2a9dc44baaab7362967e3c6368
Thomas Nilefalk <thomas@junovagen.se>
2019-03-17 00:02:55 +0100

    Const/let

--- src/components/Tunes.vue
+++ src/components/Tunes.vue
@@ -35,12 +35,12 @@ export default class Tunes extends Vue {
 
   public formatTime(seconds: number) {
     // Hours, minutes and seconds
-    var hrs = ~~(seconds / 3600);
-    var mins = ~~((seconds % 3600) / 60);
-    var secs = ~~seconds % 60;
+    const hrs = ~~(seconds / 3600);
+    const mins = ~~((seconds % 3600) / 60);
+    const secs = ~~seconds % 60;
 
     // Output like "1:01" or "4:03:59" or "123:03:59"
-    var ret = "";
+    let ret = "";
 
     if (hrs > 0) {
       ret += "" + hrs + ":" + (mins < 10 ? "0" : "");

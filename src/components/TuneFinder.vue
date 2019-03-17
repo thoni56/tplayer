79eecf98a7e18cce1fe6688b5c23430d8bebb2cf
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2019-03-17 15:56:32 +0100

    Batch updating of all tunes when loading

--- src/components/TuneFinder.vue
+++ src/components/TuneFinder.vue
@@ -1,40 +1,58 @@
 <script lang="ts">
-import { remote } from "electron";
+import {
+    remote
+} from "electron";
 import walk from "walkdir";
 import * as mm from "music-metadata";
-import { TuneInfo } from "@/models/TuneInfo";
+import {
+    TuneInfo
+} from "@/models/TuneInfo";
 
 const walker = remote.require("walkdir");
 
 export default {};
 
 function info(field: any): any {
-  return field ? field : "unknown";
+    return field ? field : "unknown";
 }
 
 export const allTunes: TuneInfo[] = [];
 const files: string[] = [];
 
 async function readMetadataForAllFiles() {
-  const all = new Array(files.length);
-  for (const index in files) {
-    if (files.hasOwnProperty(index)) {
-      // See https://stackoverflow.com/questions/40770425/tslint-codelyzer-ng-lint-error-for-in-statements-must-be-filtere
-      const metadata = await mm.parseFile(files[index]);
-      all[index] = new TuneInfo(files[index]);
-      all[index].fillFromCommonTags(metadata);
+    const all = new Array(files.length);
+    let previous = 0;
+    for (let index = 0; index < files.length; index++) {
+        console.log(files[index]);
+        if (files.hasOwnProperty(index)) {
+            // See https://stackoverflow.com/questions/40770425/tslint-codelyzer-ng-lint-error-for-in-statements-must-be-filtere
+            const metadata = await mm.parseFile(files[index]);
+            all[index] = new TuneInfo(files[index]);
+            all[index].fillFromCommonTags(metadata);
+        }
+        if (index > 0 && index % 100 == 0) {
+          allTunes.push(...all.slice(previous, index));
+          previous = index;
+        }
     }
-  }
-  allTunes.push(...all);
+    allTunes.push(...all.slice(previous));
 }
 
-const emitter = walk("C:/Users/Thomas/Music/iTunes/iTunes Media/Music");
+let emitter;
+if (process.platform == "darwin") {
+    emitter = walk("/Users/Thomas/Music/iTunes/iTunes Music/Compilations");
+} else {
+    emitter = walk("C:/Users/Thomas/Music/iTunes/iTunes Media/Music");
+}
 emitter.on("file", (path: string) => {
-  if (path.endsWith(".mp3") || path.endsWith(".m4a") || path.endsWith(".mp4")) {
-    files.push(path);
-  }
+    if (path.endsWith(".mp3") || path.endsWith(".m4a") || path.endsWith(".mp4")) {
+        files.push(path);
+    }
 });
 emitter.on("end", () => {
-  readMetadataForAllFiles();
+    readMetadataForAllFiles();
 });
+emitter.on("error", (path) => {
+  console.log("Error parsing ", path);
+})
 </script>

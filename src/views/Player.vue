7ac43c74650614312546522c158cf8e9fd623590
Thomas Nilefalk <thomas@junovagen.se>
2019-03-18 22:34:37 +0100

    Implement play and pause of first tune in list

--- src/views/Player.vue
+++ src/views/Player.vue
@@ -3,14 +3,34 @@
     <h1>PLAYER</h1>
     <PlayerControls v-on:play-track="playTrack" @pause-track="pauseTrack"/>
     <Tunes :tunes="tunes"/>
+    <audio/>
   </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import TuneInfo from "TuneInfo.vue";
+import { TuneInfo } from "@/models/TuneInfo";
 import Tunes from "@/components/Tunes.vue";
 import PlayerControls from "@/components/PlayerControls.vue";
+import { remote } from "electron";
+
+import fs from "fs";
+
+const dataurl = remote.require("dataurl");
+
+async function convertSongToUri(filePath: string): Promise<string> {
+  const songPromise = new Promise<string>((resolve, reject) => {
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(dataurl.convert({ data, mimetype: "audio/mp3" }));
+    });
+  });
+  return await songPromise;
+}
+
+const audio = new Audio();
 
 @Component({
   components: {
@@ -22,11 +42,20 @@ export default class Player extends Vue {
   @Prop() public tunes!: TuneInfo[];
 
   public playTrack() {
-    console.log("Play!");
+    convertSongToUri(this.tunes[0].file!).then(
+      uri => {
+        audio.src = uri;
+        audio.load();
+        audio.play();
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   public pauseTrack() {
-    console.log("Pause!");
+    audio.pause();
   }
 }
 </script>

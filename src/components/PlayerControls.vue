26d0a0c43bc267fb44ad469c40c2f5632e33d72c
Thomas Nilefalk <thomas@junovagen.se>
2019-03-20 07:52:40 +0100

    Unify colors

--- src/components/PlayerControls.vue
+++ src/components/PlayerControls.vue
@@ -2,13 +2,13 @@
   <div>
     <v-toolbar flat height="90">
       <v-spacer></v-spacer>
-      <v-btn outline fab small color="light-blue" @click="previousTrack">
+      <v-btn outline fab small color="#1976d2" @click="previousTrack">
         <v-icon>skip_previous</v-icon>
       </v-btn>
-      <v-btn outline fab color="light-blue" @click="playTrack">
+      <v-btn outline fab color="#1976d2" @click="playTrack">
         <v-icon large>{{ this.icon }}</v-icon>
       </v-btn>
-      <v-btn outline fab small color="light-blue" @click="$emit('play-track')">
+      <v-btn outline fab small color="#1976d2" @click="$emit('play-track')">
         <v-icon>skip_next</v-icon>
       </v-btn>
       <v-spacer></v-spacer>

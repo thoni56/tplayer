6da265560b44c93d5053d6eada26375fc0277874
Thomas Nilefalk <thomas@junovagen.se>
2019-04-06 10:59:49 +0200

    Add @vue/cli-plugin-e2e-cypress

--- cypress/integration/sample_test.js
+++ cypress/integration/sample_test.js
@@ -1,7 +1,6 @@
-describe('The App', function() {
-    it('should start and display a page', function() {
-        cy.visit('http://localhost:8080/'
-        )
-      cy.contains('Total')
-    })
+describe('The App', function () {
+  it('should start and display a page', function () {
+    cy.visit('/');
+    cy.contains('Total')
   })
+})

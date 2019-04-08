cd93b3514e3e49172ff3cedc3b32b273f4101365
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2019-04-08 17:51:28 +0200

    Remove undefined values from created tunes

--- src/models/TuneInfo.ts
+++ src/models/TuneInfo.ts
@@ -1,6 +1,5 @@
 import { IPicture } from 'music-metadata/lib/type';
 import { IAudioMetadata } from 'music-metadata';
-// import '../../public/vinyl.png';
 
 function pictureToHTML(picture: IPicture) {
   return "data:" + picture.format + ";base64," + picture.data.toString('base64');
@@ -30,19 +29,19 @@ export class TuneInfo {
     cover?: IPicture
   ) {
     this.file = file;
-    this.title = title ? title : undefined;
-    this.artist = artist ? artist : undefined;
-    this.album = album ? album : undefined;
-    this.genre = genre ? this.genresToGenres(genre) : undefined;
+    this.title = title ? title : "";
+    this.artist = artist ? artist : "";
+    this.album = album ? album : "";
+    this.genre = genre ? this.genresToGenres(genre) : [];
     this.bpm = bpm ? Math.round(bpm) : undefined;
     this.cover = cover ? pictureToHTML(cover) : defaultCover;
   }
 
   public fillFromCommonTags(metadata: IAudioMetadata) {
-    this.title = metadata.common.title;
-    this.album = metadata.common.album;
+    this.title = metadata.common.title?metadata.common.title:"";
+    this.album = metadata.common.album?metadata.common.album:"";
     this.track = metadata.common.track.no;
-    this.artist = metadata.common.artist;
+    this.artist = metadata.common.artist?metadata.common.artist:"";
     this.bpm = metadata.common.bpm ? Math.round(metadata.common.bpm) : undefined;
     this.genre = metadata.common.genre ? this.genresToGenres(metadata.common.genre) : undefined;
     this.duration = metadata.format.duration ? Math.round(metadata.format.duration) : undefined;

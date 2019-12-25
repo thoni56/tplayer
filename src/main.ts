d206e8639837bf95b1f408879a13d12db0db61fb
Thomas Nilefalk <thomas@junovagen.se>
2019-12-25 23:04:05 +0100

    Upgrade to vuetify 2.0

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,11 @@
 import Vue from 'vue';
-import './plugins/vuetify';
 import App from './App.vue';
 import '@fortawesome/fontawesome-free/css/all.css';
-import colors from 'vuetify/es5/util/colors';
-import Vuetify from 'vuetify';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-Vue.use(Vuetify, {
-  options: {
-    customProperties: true
-  },
-  theme: {
-    primary: colors.red.darken4
-  }
-});
-
 new Vue({
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,23 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
-import 'vuetify/src/stylus/app.styl'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import colors from 'vuetify/es5/util/colors';
+import { VuetifyPreset } from 'vuetify/types/presets';
 
-Vue.use(Vuetify, {
-  iconfont: 'md'
-})
+Vue.use(Vuetify)
+
+const opts: VuetifyPreset = {
+  icons: {
+    iconfont: 'md'
+  },
+  options: {
+    customProperties: true
+  },
+  themes: {
+    theme: {
+      primary: colors.red.darken4
+    }
+  }
+}
+
+export default new Vuetify(opts);

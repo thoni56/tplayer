4f1c661fa7ac5910e466558ad34236ebfecc8d0e
Thomas Nilefalk <thomas@junovagen.se>
2019-12-31 11:04:18 +0100

    Finish bpm range manipulation

--- src/views/Filtering.vue
+++ src/views/Filtering.vue
@@ -34,7 +34,7 @@
           controls-type="updown"
           v-model="bpmRange"
           @change="$emit('change-bpmRange', bpmRange);"
-          style="font-size:3vh;width:4vw;margin-right:1vw;"
+          style="font-size:3vh;width:8vw;margin-right:1vw;"
         />
       </v-flex>
       <v-flex align-self-center shrink>

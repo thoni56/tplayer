33d0b1ca727a5b80cb1069d7f1fbbf250f8c03f4
Thomas Nilefalk <thomas@junovagen.se>
2020-02-09 23:00:48 +0100

    Add some tests and fix timeFormatter (30s)

--- src/models/timeFormatter.ts
+++ src/models/timeFormatter.ts
@@ -2,7 +2,7 @@ export function formatTime(seconds: number): string {
     // Hours, minutes and seconds
     if (!seconds) return "0:00";
     const hrs = Math.round(seconds / 3600);
-    const mins = Math.round((seconds % 3600) / 60);
+    const mins = Math.floor((seconds % 3600) / 60);
     const secs = Math.round(seconds % 60);
 
     // Output like "1:01" or "4:03:59" or "123:03:59"

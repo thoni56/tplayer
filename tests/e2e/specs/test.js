f7a2f0303090b7e1cdff6941ef64f70c2dddeb84
Thomas Nilefalk <thomas@junovagen.se>
2020-02-09 22:45:37 +0100

    Minor change

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,7 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Tplayer', () => {
-  it('Should display total number of tunes in the root url', () => {
+  it('should display total number of tunes in the root url', () => {
     //cy.visit('/')
     //cy.contains('div', 'Total')
   })

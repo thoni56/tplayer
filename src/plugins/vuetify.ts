93597bada6d672bb9adfed3424ac01dabff68aab
Thomas Nilefalk <thomas@junovagen.se>
2020-06-22 19:34:30 +0100

    Use simple theming with explicit colors

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,28 +2,16 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import colors from 'vuetify/es5/util/colors';
-import { VuetifyPreset } from 'vuetify/types/services/presets';
 
 Vue.use(Vuetify)
 
-/* const opts: VuetifyPreset = {
-  icons: {
-    iconfont: 'md'
-  },
-  options: {
-    customProperties: true
-  },
+export default new Vuetify({
   theme: {
     dark: true,
     themes: {
       dark: {
         primary: colors.red.darken4
       }
-    },
-    options: {
-      customProperties: true
     }
   }
-}
- */
-export default new Vuetify();
+});

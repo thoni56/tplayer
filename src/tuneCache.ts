763439203369d9c13818f3c671187373c51a5950
Thomas Nilefalk <thomas.nilefalk@responsive.se>
2023-07-17 12:17:03 +0200

    Use stream-json to read cached tunes

--- src/tuneCache.ts
+++ src/tuneCache.ts
@@ -1,18 +1,31 @@
 import { BrowserWindow } from 'electron';
 import jsonfile from 'jsonfile';
+import { createReadStream } from 'fs';
+import { parser } from 'stream-json';
+import { streamArray } from 'stream-json/streamers/StreamArray';
 
-export function readTuneCacheAndSend(win: BrowserWindow, tuneCache: string) {
-  // TODO Responsibility to send should be pushed up to caller
-  jsonfile
-    .readFile(tuneCache)
-    .then((tunes) => win?.webContents.send('discoveredTunes', tunes))
-    .catch((error) => console.log(error));
+export function readTuneCacheAndSend(renderer: BrowserWindow, tuneCache: string) {
+  return new Promise((resolve, reject) => {
+    const result: any[] = [];
+
+    const pipeline = createReadStream(tuneCache)
+      .pipe(parser())
+      .pipe(streamArray());
+
+    pipeline.on('data', (tune) => {
+      // TODO Responsibility to send should be pushed up to caller
+      renderer.webContents.send('discoveredTunes', [tune]);
+    });
+
+    pipeline.on('end', () => resolve(result));
+    pipeline.on('error', reject);
+  });
 }
 
 import fs from 'fs';
 
-export async function writeTunesToCache(tunes: any[], path: string) {
-  const fileStream = fs.createWriteStream(path, { encoding: 'utf8' });
+export async function writeTunesToCache(tunes: any[], tunesCache: string) {
+  const fileStream = fs.createWriteStream(tunesCache, { encoding: 'utf8' });
   fileStream.write('[');
   tunes.forEach((item, index) => {
     const itemString = JSON.stringify(item);

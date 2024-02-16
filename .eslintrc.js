370fa08f97952d329abb66991928b9fa8655091c
Thomas Nilefalk <thomas@nilefalk.se>
2024-02-16 21:05:46 +0100

    Some tweaks to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es6": true
+        "browser": true, // For renderer process
+        "es6": true,
+        "node": true, // For main process
     },
-    "extends": "standard",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
+    "extends": [
+        "standard", // Standard JavaScript style guide
+        "plugin:@typescript-eslint/recommended", // TypeScript recommendations
+        "plugin:vue/recommended" // Vue recommendations
+    ],
     "parser": "vue-eslint-parser",
     "parserOptions": {
-        "ecmaVersion": 2018
+        "parser": "@typescript-eslint/parser", // Use TypeScript parser for `<script>` tags
+        "ecmaVersion": 2018,
+        "sourceType": "module", // Allows for the use of imports
     },
     "plugins": [
         "vue",
-        "vuetify"
+        "vuetify",
+        "@typescript-eslint" // Add TypeScript plugin
     ],
     "rules": {
         "vuetify/no-deprecated-classes": "warn",
-        "vuetify/grid-usage": "warn"
+        "vuetify/grid-usage": "warn",
+        // Add or adjust rules as needed for TypeScript
     }
 };

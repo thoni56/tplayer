d03e87aa6c5f2bf75967c6e353de323a6747c6f0
Thomas Nilefalk <thomas@nilefalk.se>
2024-02-19 21:00:02 +0100

    Bump to 0.12.0

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,21 @@
+const { listenerCount } = require("cluster")
+
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
   },
   pluginOptions: {
     electronBuilder: {
-      preload: 'src/preload.js'
+      preload: 'src/preload.js',
+      builderOptions: {
+        linux: {
+          target: [
+            'deb',
+            'AppImage',
+            'deb',
+          ]
+        }
+      }
     }
-  }
+  },
 }

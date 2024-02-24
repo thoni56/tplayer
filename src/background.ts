e681a8fea92a334938f3fef34be222c281e9dc3c
Thomas Nilefalk <thomas@nilefalk.se>
2024-02-24 14:12:11 +0100

    Fix errors in .prettierrc

--- src/background.ts
+++ src/background.ts
@@ -10,9 +10,7 @@ import fs from 'fs';
 import { UsedGenres } from './genres';
 
 var settings = require('user-settings').file('.tplayerrc');
-var autoloadDirectory = settings.get('autoload')
-  ? settings.get('autoload')[0]
-  : undefined;
+var autoloadDirectory = settings.get('autoload') ? settings.get('autoload')[0] : undefined;
 const userHome = app.getPath('home');
 const tuneCache = userHome + '/.tplayer_cache.json';
 
@@ -24,130 +22,129 @@ let window: BrowserWindow | null;
 
 // Standard scheme must be registered before the app is ready
 protocol.registerSchemesAsPrivileged([
-  {
-    scheme: 'app',
-    privileges: {
-      standard: true,
-      secure: true,
+    {
+        scheme: 'app',
+        privileges: {
+            standard: true,
+            secure: true,
+        },
     },
-  },
 ]);
 
 const title = 'Tplayer - ' + app.getVersion();
 
 async function createWindow() {
-  // Create the browser window.
-  window = new BrowserWindow({
-    title: title,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: process.env
-        .ELECTRON_NODE_INTEGRATION as unknown as boolean,
-      preload: path.join(__dirname, 'preload.js'),
-    },
-  });
-  window.setMenu(null);
-
-  if (process.env.WEBPACK_DEV_SERVER_URL) {
-    // Load the url of the dev server if in development mode
-    window.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
-    window.webContents.on('did-finish-load', () => {
-      if (!process.env.IS_TEST) {
-        window!.webContents.openDevTools();
-      }
+    // Create the browser window.
+    window = new BrowserWindow({
+        title: title,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION as unknown as boolean,
+            preload: path.join(__dirname, 'preload.js'),
+        },
     });
-  } else {
-    createProtocol('app');
-    // Load the index.html when not in development
-    window.setFullScreen(true);
-    window.loadURL('app://./index.html');
-  }
+    window.setMenu(null);
 
-  window.on('closed', () => {
-    window = null;
-  });
+    if (process.env.WEBPACK_DEV_SERVER_URL) {
+        // Load the url of the dev server if in development mode
+        window.loadURL(process.env.WEBPACK_DEV_SERVER_URL);
+        window.webContents.on('did-finish-load', () => {
+            if (!process.env.IS_TEST) {
+                window!.webContents.openDevTools();
+            }
+        });
+    } else {
+        createProtocol('app');
+        // Load the index.html when not in development
+        window.setFullScreen(true);
+        window.loadURL('app://./index.html');
+    }
+
+    window.on('closed', () => {
+        window = null;
+    });
 }
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', async () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (window === null) {
-    await createWindow();
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (window === null) {
+        await createWindow();
+    }
 });
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS_DEVTOOLS);
-    } catch (e: any) {
-      // tslint:disable-next-line: no-console
-      console.error('Vue Devtools failed to install:', e.toString());
+    if (isDevelopment && !process.env.IS_TEST) {
+        // Install Vue Devtools
+        try {
+            await installExtension(VUEJS_DEVTOOLS);
+        } catch (e: any) {
+            // tslint:disable-next-line: no-console
+            console.error('Vue Devtools failed to install:', e.toString());
+        }
     }
-  }
-  await createWindow();
+    await createWindow();
 });
 
 // Exit cleanly on request from parent process in development mode.
 if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit();
-      }
-    });
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit();
-    });
-  }
+    if (process.platform === 'win32') {
+        process.on('message', (data) => {
+            if (data === 'graceful-exit') {
+                app.quit();
+            }
+        });
+    } else {
+        process.on('SIGTERM', () => {
+            app.quit();
+        });
+    }
 }
 
 import { readTunesFromCache } from './tuneCache';
 ipcMain.on('renderer-ready', () => {
-  console.log('renderer-ready');
-  if (fs.existsSync(tuneCache)) {
-    console.time('readTunesFromCache'); // Start timing
-    readTunesFromCache(tuneCache).then((tunes) => {
-      window!.webContents.send('discovered-tunes', tunes);
-      console.timeEnd('readTunesFromCache'); // End timing
-    });
-  } else if (autoloadDirectory) {
-    discoverTunes(window!, autoloadDirectory, UsedGenres, tuneCache);
-  }
+    console.log('renderer-ready');
+    if (fs.existsSync(tuneCache)) {
+        console.time('readTunesFromCache'); // Start timing
+        readTunesFromCache(tuneCache).then((tunes) => {
+            window!.webContents.send('discovered-tunes', tunes);
+            console.timeEnd('readTunesFromCache'); // End timing
+        });
+    } else if (autoloadDirectory) {
+        discoverTunes(window!, autoloadDirectory, UsedGenres, tuneCache);
+    }
 });
 
 ipcMain.on('discover-tunes', () => {
-  const dir: string[] | undefined = dialog.showOpenDialogSync({
-    properties: ['openDirectory'],
-  });
-  if (dir) {
-    window?.webContents.send('clear-tunes', []);
-    discoverTunes(window!, dir[0], UsedGenres, tuneCache);
-    settings.set('autoload', dir);
-  }
+    const dir: string[] | undefined = dialog.showOpenDialogSync({
+        properties: ['openDirectory'],
+    });
+    if (dir) {
+        window?.webContents.send('clear-tunes', []);
+        discoverTunes(window!, dir[0], UsedGenres, tuneCache);
+        settings.set('autoload', dir);
+    }
 });
 
 function convertToUri(filePath: string) {
-  const uri: string = datauri.sync(filePath);
-  return uri;
+    const uri: string = datauri.sync(filePath);
+    return uri;
 }
 
 // Synchronous IPC call to convert to file path to URI
 ipcMain.handle('convertSongToUri', async (event: any, filePath: string) => {
-  const uri: string = convertToUri(filePath);
-  return uri;
+    const uri: string = convertToUri(filePath);
+    return uri;
 });
--- src/tuneFinder.ts
+++ src/tuneFinder.ts
@@ -5,67 +5,60 @@ import { TuneInfo } from '../src/models/TuneInfo';
 import { writeTunesToCache } from './tuneCache';
 
 export function discoverTunes(
-  window: BrowserWindow,
-  directory: string,
-  genres: string[],
-  tuneCache: string
+    window: BrowserWindow,
+    directory: string,
+    genres: string[],
+    tuneCache: string
 ) {
-  const files: string[] = [];
-  const emitter = walk(directory, { follow_symlinks: true });
-  window.webContents.send('progress', 0);
-  window.webContents.send('start-loading');
-  emitter.on('file', (path: string) => {
-    if (
-      path.endsWith('.aac') ||
-      path.endsWith('.m4a') ||
-      path.endsWith('.mp3') ||
-      path.endsWith('.mpga')
-    ) {
-      files.push(path);
-    }
-  });
-  emitter.on('end', () => {
-    readMetadataForAllFiles(window, files, genres).then(async (tunes) => {
-      window.webContents.send('discovered-tunes', tunes);
-      writeTunesToCache(tunes, tuneCache)
-        .then(() => window.webContents.send('finished-loading'))
-        .catch((error) => console.log('Error caching tunes:' + error));
+    const files: string[] = [];
+    const emitter = walk(directory, { follow_symlinks: true });
+    window.webContents.send('progress', 0);
+    window.webContents.send('start-loading');
+    emitter.on('file', (path: string) => {
+        if (
+            path.endsWith('.aac') ||
+            path.endsWith('.m4a') ||
+            path.endsWith('.mp3') ||
+            path.endsWith('.mpga')
+        ) {
+            files.push(path);
+        }
     });
-  });
-  emitter.on('error', (path: string) => {
-    // tslint:disable-next-line:no-console
-    console.log("Error parsing '", path, "'");
-  });
+    emitter.on('end', () => {
+        readMetadataForAllFiles(window, files, genres).then(async (tunes) => {
+            window.webContents.send('discovered-tunes', tunes);
+            writeTunesToCache(tunes, tuneCache)
+                .then(() => window.webContents.send('finished-loading'))
+                .catch((error) => console.log('Error caching tunes:' + error));
+        });
+    });
+    emitter.on('error', (path: string) => {
+        // tslint:disable-next-line:no-console
+        console.log("Error parsing '", path, "'");
+    });
 }
 
-async function readMetadataForAllFiles(
-  window: BrowserWindow,
-  files: string[],
-  genres: string[]
-) {
-  let all = [];
-  let totalProgress = files.length;
-  let actualProgress = 0;
+async function readMetadataForAllFiles(window: BrowserWindow, files: string[], genres: string[]) {
+    let all = [];
+    let totalProgress = files.length;
+    let actualProgress = 0;
 
-  for (let index = 0; index < files.length; index++) {
-    try {
-      const metadata = await musicMetaData.parseFile(files[index]);
-      const tuneInfo = new TuneInfo(files[index]);
-      tuneInfo.fillFromCommonTags(metadata);
-      if (genres.some((g) => tuneInfo.genre && tuneInfo.genre.includes(g))) {
-        all.push(tuneInfo);
-        actualProgress++;
-      } else {
-        totalProgress--;
-      }
-    } catch (e) {
-      // tslint:disable-next-line: no-console
-      console.log('*** Could not read metadata from ', files[index]);
+    for (let index = 0; index < files.length; index++) {
+        try {
+            const metadata = await musicMetaData.parseFile(files[index]);
+            const tuneInfo = new TuneInfo(files[index]);
+            tuneInfo.fillFromCommonTags(metadata);
+            if (genres.some((g) => tuneInfo.genre && tuneInfo.genre.includes(g))) {
+                all.push(tuneInfo);
+                actualProgress++;
+            } else {
+                totalProgress--;
+            }
+        } catch (e) {
+            // tslint:disable-next-line: no-console
+            console.log('*** Could not read metadata from ', files[index]);
+        }
+        window.webContents.send('progress', Math.round((actualProgress / totalProgress) * 100));
     }
-    window.webContents.send(
-      'progress',
-      Math.round((actualProgress / totalProgress) * 100)
-    );
-  }
-  return all;
+    return all;
 }
